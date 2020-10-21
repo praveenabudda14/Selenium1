4df92cce158aec02218b393d0e10a6194137481c
Kalyan Veena <Kalyan Veena@LAPTOP-IV74DSNU>
2020-10-21 13:55:14 +0530

    2nd commit

---
+++ src/Student.java
@@ -0,0 +1,17 @@
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="veena";
+		String r="";
+	
+		for(int i=s.length()-1;i>=0;i--) {
+			r=r+s.charAt(i);
+			
+			
+		}
+		System.out.println(r);
+		}
+
+}
